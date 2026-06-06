7fddced68f95d12a78d187a9be48fbdcc5a7e6dd
Sujal Choudhary <chsujal8@gmail.com>
2026-06-06 16:09:32 +0530

    first commit

---
+++ src/main/java/com/sujal/mesh_pay/MeshPayApplication.java
@@ -0,0 +1,13 @@
+package com.sujal.mesh_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeshPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeshPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sujal/mesh_pay/MeshPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujal.mesh_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeshPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
