74e269be9a9c807c5f680f84d8072fe478242e04
Sujal Choudhary <chsujal8@gmail.com>
2026-06-08 22:03:23 +0530

    created Transaction entity and added getters and setters to account class

--- src/main/java/com/sujal/mesh_pay/entity/Account.java
+++ src/main/java/com/sujal/mesh_pay/entity/Account.java
@@ -19,4 +19,46 @@ public class Account {
 
     @Version  // Optimistic locking — prevents lost updates on concurrent transfers
     private Long version;
+
+    public Account() {
+    }
+
+    public Account(String vpa, String holderName, BigDecimal balance) {
+        this.vpa = vpa;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    public String getVpa() {
+        return vpa;
+    }
+
+    public void setVpa(String vpa) {
+        this.vpa = vpa;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public void setHolderName(String holderName) {
+        this.holderName = holderName;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
 }
