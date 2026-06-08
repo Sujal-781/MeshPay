8e2fd538bdbd73adefd36e88e0dd8ae7a27f9d42
Sujal Choudhary <chsujal8@gmail.com>
2026-06-08 22:11:52 +0530

    created repositories for Account and Transaction entities

---
+++ src/main/java/com/sujal/mesh_pay/repository/AccountRepository.java
@@ -0,0 +1,7 @@
+package com.sujal.mesh_pay.repository;
+
+import com.sujal.mesh_pay.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, String> {
+}
